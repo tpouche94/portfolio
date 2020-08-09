6377b9d116e9fc68f04be0614eca5a952311afef
tpouche94 <jordanjoseph94@yahoo.com>
2020-08-09 17:40:44 -0400

    polish

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def allblogs(request):
     blogs = Blog.objects
     return render(request, 'allblogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html', {'blog': detailblog})
+
